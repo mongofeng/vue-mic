ab6bcd60b550d95c2e1232cdd53776fb83893440
yangpu <kk.yp@mobvoyage.com>
2020-08-25 17:49:50 +0800

    feat: 添加singlespa的环境

--- packages/root-html-file/app.js
+++ packages/root-html-file/app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const path = require('path')
+var history = require('connect-history-api-fallback');
 var app = express();
+app.use(history(
+  {
+    logger: console.log.bind(console)
+  }
+));
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.listen(3000, function () {
