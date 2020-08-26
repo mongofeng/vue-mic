67257320e17a8d10efadfa5067a8a8a69a6871fb
yangpu <kk.yp@mobvoyage.com>
2020-08-26 15:05:26 +0800

    feat: 抽取各应用的配置

---
+++ build/generate-config.js
@@ -0,0 +1,66 @@
+/** @format */
+const path = require('path')
+const argv = require('minimist')(process.argv.slice(2))
+
+module.exports = function(process, dirname) {
+  const isProduction = process.env.NODE_ENV === 'production'
+  const appName = process.env.VUE_APP_NAME
+  const port = process.env.port
+  const basePath = argv['base-path'] || '/'
+
+  return {
+    publicPath: isProduction ? `${basePath}${appName}/` : `http://localhost:${port}/`,
+
+    outputDir: path.resolve(dirname, `../../dist/${appName}`),
+
+    // css: {
+    //   loaderOptions: {
+    //     less: {
+    //       modifyVars: {
+    //         'primary-color': '#FF9F08',
+    //         'link-color': '#FF9F08',
+    //         'body-background': '#FCFBF9',
+    //       },
+    //       javascriptEnabled: true,
+    //     },
+    //     sass: {
+    //       prependData: `@import "~@root/common/styles/settings.scss";`,
+    //     },
+    //   },
+    // },
+
+    configureWebpack: config => {
+      config.devServer = {
+        port,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+    },
+
+    chainWebpack: config => {
+      // 根目录别名
+      // config.resolve.alias.set('@root', path.resolve(dirname, '../../'))
+      // // .set('@ant-design-vue/icons/lib/dist$', path.resolve(__dirname, './src/assets/icons.ts'))
+
+      // // 公用的第三方库不参与打包
+      // config.externals([
+      //   'vue',
+      //   'vue-router',
+      //   'vuex',
+      //   'axios',
+      //   'echarts',
+      //   'lodash',
+      //   { moment: 'moment' },
+      //   { '../moment': 'moment' }, // 这句很关键，在 moment 内置语种包中通过 ../moment 来调用 moment 的方法，所以也需要将这个设置为外置引用 window.moment
+      // ])
+
+      config.output.library(appName).libraryTarget('umd')
+
+      if (isProduction) {
+        // 打包目标文件加上 hash 字符串，禁止浏览器缓存
+        config.output.filename('js/index.[hash:8].js')
+      }
+    },
+  }
+}
--- packages/app1/vue.config.js
+++ packages/app1/vue.config.js
@@ -1,35 +1,3 @@
-// Temporary until we can use https://github.com/webpack/webpack-dev-server/pull/2143
-const path = require('path')
-const isProduction = process.env.NODE_ENV === 'production'
-const baseUrl = process.env.VUE_APP_BASE_URL
-const appName = process.env.VUE_APP_NAME
-module.exports = {
-  publicPath: isProduction ? `${baseUrl}${appName}/` : `http://localhost:${process.env.port}/`,
+const generateConfig = require('../../build/generate-config')
 
-
-  outputDir: path.resolve(__dirname, `../../dist/${appName}`),
-
-  devServer: {
-    port: process.env.port,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
-
-
-
-
-  chainWebpack: config => {
-    config.devServer.set('inline', false)
-    config.devServer.set('hot', true)
-    // Vue CLI 4 output filename is js/[chunkName].js, different from Vue CLI 3
-    // More Detail: https://github.com/vuejs/vue-cli/blob/master/packages/%40vue/cli-service/lib/config/app.js#L29
-    if (process.env.NODE_ENV !== 'production') {
-      config.output.filename(`js/[name].js`)
-    }
-    config.externals(['vue', 'vue-router'])
-
-    config.output.library(appName).libraryTarget('umd')
-  },
-  // filenameHashing: false
-}
+module.exports = generateConfig(process, __dirname)
