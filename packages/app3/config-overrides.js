b185b143fc164129636aa325087333c8c3966b14
yangpu <kk.yp@mobvoyage.com>
2020-08-28 17:17:12 +0800

    feat: 完成react的代码切割

--- packages/app3/config-overrides.js
+++ packages/app3/config-overrides.js
@@ -1,13 +1,27 @@
 /* config-overrides.js */
 const fs = require('fs')
 const path = require('path')
+// https://github.com/arackaf/customize-cra
+ const adjustStyleLoaders = (config) => {
+  const mode = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+  const loader = mode === 'prod' ? 'css-extract-plugin' : 'style-loader';
+
+  const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
+    .oneOf;
+  const styleLoaders = loaders.filter(({ use }) => use && use[0] && (use[0].loader || use[0]).includes(loader));
+  styleLoaders.forEach(loader => {
+    const {use} = loader
+    use[0] = require.resolve('style-loader')
+  });
+
+  return config;
+};
 
 module.exports = {
-  webpack: function(config, env) {
+  webpack: function (config, env) {
     console.log('当前环境>>', env)
     // fs.writeFileSync(`${env}.json`, JSON.stringify(config))
     // console.log('配置导出完成')
-    // console.log(process.env.PORT)
     const baseUrl = '/'
     const isProduction = env === 'production'
     const appName = process.env.REACT_APP_NAME
@@ -18,8 +32,8 @@ module.exports = {
     const filename = isProduction ? 'js/app.[hash:8].js' : 'js/app.js'
     // const system
 
-      // 构件库信息
-      // https://single-spa.js.org/docs/faq/#create-react-app
+    // 构件库信息
+    // https://single-spa.js.org/docs/faq/#create-react-app
     config.output = {
       path: path.resolve(__dirname, './build'),
       publicPath,
@@ -30,10 +44,12 @@ module.exports = {
     // 修改webpack optimization属性，删除代码分割逻辑
     // delete config.optimization.splitChunks;
     // delete config.optimization.runtimeChunk;
-      
+
     config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'HtmlWebpackPlugin')
+
+    adjustStyleLoaders(config)
+
     delete config.optimization
-    console.log(config)
     return config;
   },
   devServer(configFunction) {
@@ -46,26 +62,3 @@ module.exports = {
     }
   }
 }
-
-// module.exports = {
-//   webpack(config, env) {
-//     // config.entry = './src/single-spa-entry.js';
-//     config.output = {
-//       ...config.output,
-//       filename: 'project-name.js',
-//       libraryTarget: 'system',
-//     }
-//     config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'HtmlWebpackPlugin')
-//     delete config.optimization
-//     return config;
-//   },
-//   devServer(configFunction) {
-//     return function (proxy, allowedHost) {
-//       const config = configFunction(proxy, allowedHost);
-//       config.disableHostCheck = true
-//       config.headers = config.headers || {}
-//       config.headers['Access-Control-Allow-Origin'] = '*'
-//       return config
-//     }
-//   }
-// }
